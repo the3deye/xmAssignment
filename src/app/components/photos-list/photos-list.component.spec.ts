2a8505b678a51a38e9834b97c477c5843c3a8af2
George Pyliaros <george.pyliaros@agileactors.com>
2022-09-06 20:41:57 +0300

    unit testing part 2

--- src/app/components/photos-list/photos-list.component.spec.ts
+++ src/app/components/photos-list/photos-list.component.spec.ts
@@ -1,17 +1,51 @@
-import { MatDialogModule } from '@angular/material/dialog';
+import { PicsumPhoto } from './../../interfaces/picsumPhoto';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PhotosListComponent } from './photos-list.component';
 import { ServiceService } from 'src/app/services/service.service';
+import { AddFavoritesDialogComponent } from '../common/add-favorites-dialog/add-favorites-dialog.component';
+
+const mockSessionStorageData:any = {
+  0:'https://picsum.photos/g/200/300',
+  10:'https://picsum.photos/g/200/300',
+  120:'https://picsum.photos/g/200/300',
+  1750:'https://picsum.photos/g/200/300',
+}
+
+const photoInStorage: PicsumPhoto = {
+  author: 'Alejandro Escamilla',
+    author_url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    filename: '0.jpeg',
+    format: 'jpeg',
+    height: 3744,
+    id: 120,
+    post_url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    width: 5616,
+}
+const photoNotInStorage: PicsumPhoto = {
+  author: 'Alejandro Escamilla',
+    author_url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    filename: '0.jpeg',
+    format: 'jpeg',
+    height: 3744,
+    id: 1000,
+    post_url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    width: 5616,
+}
 
 describe('PhotosListComponent', () => {
   let component: PhotosListComponent;
   let fixture: ComponentFixture<PhotosListComponent>;
+  let store: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule, MatDialogModule ],
-      providers: [ServiceService],
+      providers: [
+        ServiceService,
+        MatDialog
+      ],
       declarations: [ PhotosListComponent ]
     })
     .compileComponents();
@@ -19,9 +53,60 @@ describe('PhotosListComponent', () => {
     fixture = TestBed.createComponent(PhotosListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = mockSessionStorageData;
+    const mockSessionStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      }
+    };
+    spyOn(sessionStorage, 'getItem').and.callFake( mockSessionStorage.getItem );
+    spyOn(sessionStorage, 'setItem').and.callFake( mockSessionStorage.setItem );
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('on image click should add in sessionStorage and open success dialog', ()=> {
+    const openDialogSpy = spyOn(component.dialog, 'open')
+
+    expect(store).toEqual(mockSessionStorageData);
+    component.onImgClick(photoNotInStorage);
+    expect(store[photoNotInStorage.id]).toBeDefined();
+    expect(store[photoNotInStorage.id]).toBe('https://picsum.photos/g/200/300');
+    expect(openDialogSpy).toHaveBeenCalledWith(AddFavoritesDialogComponent, 
+     {
+      width: '250px',
+      enterAnimationDuration: '1000ms', 
+      exitAnimationDuration: '1000ms',
+      data: {
+        success: true
+      }
+     });
+  })
+
+  it('on image click should not add in sessionStorage and should open failure dialog', ()=> {
+    const openDialogSpy = spyOn(component.dialog, 'open')
+    expect(store).toEqual(mockSessionStorageData);
+    component.onImgClick(photoInStorage);
+    expect(store[photoInStorage.id]).toBeDefined();
+    expect(store[photoInStorage.id]).toBe('https://picsum.photos/g/200/300');
+    expect(openDialogSpy).toHaveBeenCalledWith(AddFavoritesDialogComponent, 
+     {
+      width: '250px',
+      enterAnimationDuration: '1000ms', 
+      exitAnimationDuration: '1000ms',
+      data: {
+        success: false
+      }
+     });
+  })
+
+  // logo ths lathos logikis pou exei ftiaxtei to photos-list na pairnei photos apo to api tou unsplash
+  // den einai eykolo kai themito na ftiaxtoun test
+
 });
--- src/app/components/photos-list/photos-list.component.ts
+++ src/app/components/photos-list/photos-list.component.ts
@@ -20,6 +20,16 @@ export class PhotosListComponent implements OnInit {
     public dialog: MatDialog
   ) {}
 
+  // To api tou unsplash prosferei dyo urls na xtyphseis
+  // to 'https://picsum.photos/g/200/300' sou epistrefei ena random pic to opoio omws otan to xrhsimopoieis polles fores
+  // px sto render ths listas epistrefei to idio pic
+  // kai to 'https://picsum.photos/list' to opoio san api epistrefei mia alfa plhroforia me id klp alla den yparxei pouthena
+  // kapoio url pedio pou na einai ths ypotithemenhs fwtografias
+  // Egine prospathia na vrethei kapoio allo free api pou na mhn thelei idiaitero set up alla htan paromoiou typou
+
+  // Me liga logia sthn efarmogh parolo pou oses eikones kai na fainontai tha einai idies 
+  // h logikh einai xtismenh apo pisw leptomerws px den mporeis na valeis thn photo me idio id ksana sta favorites
+
   ngOnInit(): void {
     this.getPhotos();
   }
@@ -27,7 +37,6 @@ export class PhotosListComponent implements OnInit {
   private getPhotos() {
     this.service.getPhotos().subscribe(
       (res) => {
-        console.log('got answer');
         this.picsumPhotos = res;
         this.displayedPhotos = [];
         this.displayedPhotos.push(...this.picsumPhotos.slice(0, 9));
@@ -42,7 +51,6 @@ export class PhotosListComponent implements OnInit {
   onScroll(): void {
     if (window.innerHeight + window.scrollY >= document.body.scrollHeight) { //check scrolled to bottom
       this.showSpinner = true;
-      console.log('asd');
       this.timer = setTimeout(() => {
         this.displayedPhotos.push(...this.picsumPhotos.slice(0, 9));
         clearTimeout(this.timer);
@@ -52,7 +60,7 @@ export class PhotosListComponent implements OnInit {
   }
 
   private randomDelay() {
-    return Math.random() * 5000;
+    return (Math.random() + 1) * 200;
   }
 
   onImgClick(photo:PicsumPhoto) {
@@ -65,15 +73,15 @@ export class PhotosListComponent implements OnInit {
   }
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string, success: boolean): void {
-    const dialogRef = this.dialog.open(AddFavoritesDialogComponent, {
+    /*const dialogRef = */this.dialog.open(AddFavoritesDialogComponent, {
       width: '250px',
       enterAnimationDuration, 
       exitAnimationDuration,
       data: {success}
     });
 
-    dialogRef.afterClosed().subscribe(
-      result => {
-    });
+    // dialogRef.afterClosed().subscribe(
+    //   result => {
+    // });
   }
 }
